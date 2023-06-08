842591d86151d372337740d03740486112fda98b
mutantcake <128691258+mutantcake@users.noreply.github.com>
2023-06-08 12:50:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function confirmerSuppression(id) {
   if (confirm("Êtes-vous sûr de vouloir supprimer cet apprenant ?")) {
       window.location.href = "delete-apprenant.php?id=" + id;
   }
+}
   
 function confirmerModif(form) {
     if (window.confirm("Êtes-vous sûr de vouloir modifier cet apprenant ?")) {
