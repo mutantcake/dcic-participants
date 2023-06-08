48273226f0d5b4ccec9914b6c493c48ef78cd007
mutantcake <128691258+mutantcake@users.noreply.github.com>
2023-06-08 11:37:23 +0000

    Script pour les confirmations

---
+++ script.js
@@ -0,0 +1,10 @@
+function confirmerSuppression(id, message) {
+  if (confirm("Êtes-vous sûr de vouloir supprimer cet apprenant ?")) {
+      window.location.href = "delete-apprenant.php?id=" + id;
+  }
+  
+function confirmerModif(form) {
+    if (window.confirm("Êtes-vous sûr de vouloir modifier cet apprenant ?")) {
+        form.submit();
+    }
+}
