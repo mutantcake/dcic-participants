9161c0814ba0cf7ce21c662f09e0c9a95bc12cc5
mutantcake <128691258+mutantcake@users.noreply.github.com>
2023-06-08 12:49:29 +0000

    Ajout de confirmation avant modification

--- admin/edit-apprenant.php
+++ admin/edit-apprenant.php
@@ -51,11 +51,12 @@ while($row= mysqli_fetch_assoc($recup)){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
+    <script src="../script.js"></script>
     <title>Editer un apprenant</title>
 </head>
 <body>
     
-    <form action="" method="post">
+    <form action="" method="post" onsubmit="event.preventDefault(); confirmerModif(this);">
         <h1>Editer un apprenant</h1>
         <input type="text" name="nom" value="<?=$row['nom']?>" placeholder="Nom de l'apprenant" required>
         <input type="text" name="prenom" value="<?=$row['prenom']?>" placeholder="Prénom de l'apprenant" required>
@@ -64,7 +65,7 @@ while($row= mysqli_fetch_assoc($recup)){
         <input type="text" name="formation" value="<?=$row['formation']?>" placeholder="Formation de base" required>
         <input type="submit" value="Modifier" name="send">
         <a class="link back" href="liste-apprenants.php">Annuler</a>
-    </form>
+     </form>
 <?php
 }
 ?>
